a01416d1107a782bbf52f486e03158e05aedc43d
englerjose <englerjosegithub@gmail.com>
2015-06-10 21:40:20 +1200

    Complete the accessor

--- ruby-calisthenics/lib/attr_accessor_with_history.rb
+++ ruby-calisthenics/lib/attr_accessor_with_history.rb
@@ -4,7 +4,11 @@ class Class
     attr_reader attr_name # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
     class_eval %Q{
-      # YOUR CODE HERE
+      def #{attr_name}=(value)
+        @#{attr_name}_history ||= []
+        @#{attr_name}_history << @#{attr_name}
+        @#{attr_name} = value
+      end
     }
   end
 end
